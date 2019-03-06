2f6aec414434fe7c653ff977a00ec0d484395e3b
Jackson Chen <a@chenguowei.com>
2019-03-06 11:13:47 +0900

    a function that changing the text after click the button

--- src/index.jsx
+++ src/index.jsx
@@ -1,59 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-class UserDetail extends React.Component {
-  render() {
-    const img_avast = <img src='{this.props.avator}' alt={this.props.user.name}></img>;
-    return (
-      <React.Fragment>
-        <h1>{this.props.user.name}</h1>
-
-      </React.Fragment>
-    );
-  }
-}
-
-function Nav(props) {
-  return (
-    <React.Fragment>
-      <h1>Nav</h1>
-      <UserDetail user={props.user} />
-    </React.Fragment>
-  );
-}
-
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { date: new Date() };
+    this.state = {
+      isOn: false
+    };
+    // this.handleClick = this.handleClick.bind(this);
   }
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
+  handleClick() {
+    this.setState(
+      state => ({
+        isOn: !state.isOn
+      })
     );
   }
 
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
-  }
-
   render() {
-    this.author = {
-      name: "Jackson",
-      avator: `http://chenguowei.com/img/logo.png`,
-    }
     return (
       <div>
-        <Nav user={this.author} />
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <button onClick={(e) => this.handleClick(e)}>
+          {this.state.isOn ? 'ON' : 'OFF'}
+        </button>
       </div>
     );
   }
